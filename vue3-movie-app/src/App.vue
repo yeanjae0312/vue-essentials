12635c50bb9d645bafdb693108dbdd3f1cab3eea
yeanjae0312 <yeanjae@naver.com>
2022-04-19 14:35:54 +0900

    Chore router 설정

--- vue3-movie-app/src/App.vue
+++ vue3-movie-app/src/App.vue
@@ -1,19 +1,3 @@
 <template>
-  <h1>{{ message }}</h1>
-  <HelloWorld />
+  <RouterView />
 </template>
-
-<script>
-import HelloWorld from '~/components/HelloWorld' // '~/'는 webpack.config.js에서 경로별칭으로 설정해줌
-
-export default {
-  components: {
-    HelloWorld
-  },
-  data() {
-    return {
-      message: 'Hello Vue!!!!'
-    }
-  }
-}
-</script>
---
+++ vue3-movie-app/src/routes/index.js
@@ -0,0 +1,20 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import Home from './Home'
+import About from './About'
+
+export default createRouter({
+  // Hash, History 두가지 모드를 사용 가능 (우리는 Hash 사용 - 페이지를 새로고침 할 때 페이지를 찾을 수 없다는 오류를 방지)
+  // Hash => https://google.com/#/search => #이 url에 포함됨
+  history: createWebHashHistory(),
+  // pages
+  routes: [
+    {
+      path: '/',
+      component: Home
+    },
+    {
+      path: '/about',
+      component: About
+    }
+  ]
+})
