55a3a077ec40be1555198bc5adeb6b66f8a280d7
yeanjae0312 <yeanjae@naver.com>
2022-04-19 16:21:45 +0900

    Feat movie-app 헤더 추가

---
+++ vue3-movie-app/src/components/Header.vue
@@ -0,0 +1,45 @@
+<template>
+  <header>
+    <div class="nav nav-pills">
+      <div
+        v-for="nav in navigations"
+        :key="nav.name" 
+        class="nav-item">
+        <!-- active-class는 기존의 router 액티브 효과인 'router-link-active'를 부트스트랩의 액티브 효과인 'active'로 변경하겠다 -->
+        <RouterLink
+          :to="nav.href"
+          active-class="active"
+          class="nav-link">
+          {{ nav.name }}
+        </RouterLink>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      navigations: [
+        {
+          name: 'Search',
+          href: '/'
+        },
+        {
+          name: 'Movie',
+          href: '/movie'
+        },
+        {
+          name: 'About',
+          href: '/about'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
