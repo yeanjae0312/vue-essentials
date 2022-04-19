42f8562cd0bd95892df491b15c758530d54537ac
yeanjae0312 <yeanjae@naver.com>
2022-04-19 14:00:10 +0900

    init vue3-movie-app

---
+++ vue3-movie-app/.eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true
+  },
+  // 코드 규칙
+  extends: [
+    // vue
+    //'plugin:vue/vue3-essential', // Lv1
+    'plugin:vue/vue3-strongly-recommended', // Lv2
+    //'plugin:vue/vue3-recommended', // Lv3
+    
+    // js
+    'eslint:recommended'
+  ],
+  // 코드 분석기
+  parserOptions: {
+    // parser: 'babel-eslint',
+    // parser: 'vue-eslint-parser',
+  },
+  // 사용자 지정 규칙
+  rules: {
+    'vue/html-closing-bracket-newline': ['error', {
+      'singleline': 'never',
+      'multiline': 'never'
+    }],
+    "vue/html-self-closing": ["error", {
+      "html": {
+        "void": "always",
+        "normal": "never",
+        "component": "always"
+      },
+      "svg": "always",
+      "math": "always"
+    }]
+    
+  }
+}
