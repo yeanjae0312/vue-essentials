20c26527f740c92729bdcec879cc9e0a2551c1ec
yeanjae0312 <yeanjae@naver.com>
2022-04-20 17:15:43 +0900

    Feat movie-app 영화검색 추가요청 기능 추가

--- vue3-movie-app/src/store/movie.js
+++ vue3-movie-app/src/store/movie.js
@@ -26,7 +26,7 @@ export default {
   },
   // 비동기
   actions: {
-    async searchMovies({ commit }, payload) {
+    async searchMovies({ state, commit }, payload) {
       // Search movies...
       // https://www.omdbapi.com/ 사이트 영화api 사용
       const { title, type, number, year } = payload
@@ -38,6 +38,26 @@ export default {
       commit('updateState', {
         movies: Search,
       })
+      
+      console.log(totalResults) // 305 => 31번 요청해야 함
+      console.log(typeof totalResults) // string
+
+      const total = parseInt(totalResults, 10) // 10진법의 정수로 변환
+      const pageLength = Math.ceil(total / 10)
+
+      //  추가 요청
+      if (pageLength > 1) {
+        for (let page=2; page<=pageLength; page++) {
+          if (page > (number / 10)) break // number를 10보다 나눈 값보다 크면 for문을 멈춘다
+          
+          // page 숫자가 달라짐
+          const res = await axios.get(`https://www.omdbapi.com/?i=tt3896198&apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`);
+          const { Search } = res.data
+          commit('updateState', {
+            movies: [...state.movies, ...Search] // 기존의 movies 뒤에 Search를 붙이겠다.
+          })
+        }
+      }
     }
   },
 }
