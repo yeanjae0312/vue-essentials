36ab9a34675d643cfa63c1f4c8812ec4932a640b
yeanjae0312 <yeanjae@naver.com>
2022-04-20 15:28:27 +0900

    Add movie-app 리스트 컴포넌트 추가

--- vue3-movie-app/src/routes/Home.vue
+++ vue3-movie-app/src/routes/Home.vue
@@ -2,17 +2,20 @@
   <div>
     <Headline />
     <Search />
+    <MovieList />
   </div>
 </template>
 
 <script>
 import Headline from '../components/Headline'
 import Search from '../components/Search'
+import MovieList from '../components/MovieList'
 
 export default {
   components: {
     Headline,
-    Search
+    Search,
+    MovieList
   }
 }
 </script>
