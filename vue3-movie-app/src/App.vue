1805a890d0daa0dcc9b1e5ad7c0c675172f281d0
yeanjae0312 <yeanjae@naver.com>
2022-04-21 17:51:55 +0900

    Add movie-app footer 추가

--- vue3-movie-app/src/App.vue
+++ vue3-movie-app/src/App.vue
@@ -2,14 +2,18 @@
   <div>
     <Header />
     <RouterView />
+    <Footer />
   </div>
 </template>
 
 <script>
 import Header from '~/components/Header'
+import Footer from '~/components/Footer'
+
 export default {
   components: {
-    Header
+    Header,
+    Footer
   }
 }
 </script>
